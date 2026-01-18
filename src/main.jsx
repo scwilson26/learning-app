2925041e40762e659dabbc0c6f887e0ce1c5d881
Sean Wilson <sean.wilson1986@gmail.com>
2026-01-18 12:03:16 -0500

    Revert all changes to test mobile

--- src/main.jsx
+++ src/main.jsx
@@ -1,40 +1,10 @@
-import { StrictMode, Component } from 'react'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-// Error boundary to show errors on screen (for mobile debugging)
-class ErrorBoundary extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { error: null }
-  }
-
-  static getDerivedStateFromError(error) {
-    return { error }
-  }
-
-  render() {
-    if (this.state.error) {
-      return (
-        <div style={{ padding: 20, color: 'red', fontFamily: 'monospace', fontSize: 14 }}>
-          <h2>Something crashed:</h2>
-          <pre style={{ whiteSpace: 'pre-wrap', wordBreak: 'break-word' }}>
-            {this.state.error.toString()}
-            {'\n\n'}
-            {this.state.error.stack}
-          </pre>
-        </div>
-      )
-    }
-    return this.props.children
-  }
-}
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ErrorBoundary>
-      <App />
-    </ErrorBoundary>
+    <App />
   </StrictMode>,
 )
